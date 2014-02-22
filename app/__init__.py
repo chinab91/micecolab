2f70257f944846a5e32db4e2934b2d5644af16bf
chinab91 <chinab91@gmail.com>
2014-02-22 16:37:08 +0800

    moving to a /route

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.mail import Mail  # module to send email, used to support password recovery
 
-
 from config import SQLALCHEMY_DATABASE_URI
-
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,10 +10,6 @@ db = SQLAlchemy(app)
 
 mail = Mail(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 from views import general
 
-app.register_blueprint(general.mod, url_prefix='/general')
+app.register_blueprint(general.mod)
--- app/views/general.py
+++ app/views/general.py
@@ -1,6 +1,13 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, render_template
+from app import db
+#from app.models import (Attendee_Login, Attendee_Profile, Attendee_Match, Event_Info, Timestamp_Lookout
+
 mod = Blueprint('general', __name__)
 
-@mod.route('/sms', methods=['GET'])
-def get_sms_details():
-	return '1'
+@mod.route('/')
+def index():
+    return render_template('index.html')
+
+@mod.route('/get_meetings')
+def get_meetings():
+	return 'get meetings';
