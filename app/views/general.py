ca992e2ce31604bbbf660af30f2f1991eb047051
andriano winatra <andriano_winatra@gmail.com>
2014-02-22 16:47:50 +0800

    commit passcode checker

--- app/views/general.py
+++ app/views/general.py
@@ -1,6 +1,11 @@
 from flask import jsonify, Blueprint
 mod = Blueprint('general', __name__)
 
-@mod.route('/sms', methods=['GET'])
-def get_sms_details():
-	return '1'
+@mod.route('/passcode_check/<passcode_string>')
+def check_passcode(passcode_string):
+	passcode_file = open('app/views/passcode.txt')
+	passcode = passcode_file.readline()
+	if passcode_string != passcode:
+		return "wrong passcode" # pending retrun template
+	else:
+		return "right passcode" # pending return template
