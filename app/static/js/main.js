167f671b3de15f220128f3e4fb8fc219827b61d8
rtexal <yan@jublia.com>
2014-02-22 17:20:37 +0800

    Adding Meetings AJAX

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
 	$('label').click(function() {});
-	
+	$.ajax({
+	  url: "/get_meetings",
+	  dataType: "json",
+	}).done(function(mm) {
+		else {
+			$.each(mm.date, function( k, a ) {
+			  $('#panlm').append('<div class="mlfix row"><div class="col-xs-11"><p class="f13 meetm">'+a.value+'</p></div></div>');
+				$.each(a.meetings, function( k, b ) {
+					$('#panlm').append('<div class="mlfix row"><div class="col-xs-2 f13 c">'+b.time+'</div><div class="col-xs-6"><p class="f13 meetm">'+b.name+'</p><p>'+b.position+' from '+b.company+'</p></div><div class="col-xs-3 f13 c">'+b.location+'</div></div>');
+				});
+			});
+		});
 });
