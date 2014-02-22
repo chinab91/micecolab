b7c11008f0f952a325dc761ddf946e8f7a1ca782
chinab91 <chinab91@gmail.com>
2014-02-22 20:21:42 +0800

    models updated

--- app/models.py
+++ app/models.py
@@ -1,9 +1,16 @@
-from app import db
+from app import app, db
+import flask.ext.whooshalchemy as whooshalchemy
 
 class Exhibitor_Profile(db.Model):
     __tablename__ = 'exhibitor_profile'
+    
+    __searchable__ = ['location']
+
     id_exhibitor_profile = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(100))
     companyname = db.Column(db.String(100))
     position = db.Column(db.String(100))
     location = db.Column(db.String(100))
+
+whooshalchemy.whoosh_index(app, Exhibitor_Profile)
+
