0cb48976cc6df910173af2b393cf9f8076b069ba
chinab91 <chinab91@gmail.com>
2014-02-23 00:36:25 +0800

    search completed, thanks to mr. andriano

--- app/views/search.py
+++ app/views/search.py
@@ -7,6 +7,18 @@ mod = Blueprint('search', __name__)
 
 @mod.route('/<search_id>', methods=['GET'])
 def index(search_id):
+	search_results = db.session.query(Exhibitor_Profile).filter((Exhibitor_Profile.location.like('%%%s%%' % search_id)) | (Exhibitor_Profile.fullname.like('%%%s%%' % search_id)) | (Exhibitor_Profile.companyname.like('%%%s%%' %  search_id)) | (Exhibitor_Profile.position.like('%%%s%%' %  search_id))).all()
+	#pprint.pprint(search_results)
 	
-	pprint.pprint(a)
-	return '1'
+	search = []
+	for search_result in search_results:
+		search_element = {}
+		search_element['fullname'] = search_result.fullname
+		search_element['position'] = search_result.position
+		search_element['companyname'] = search_result.companyname
+		search_element['location'] = search_result.location
+		search.append(search_element)
+
+	return jsonify({"search": search})
+
+
